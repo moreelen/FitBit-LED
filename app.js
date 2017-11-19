d769dd2ed39480af0a3c551fda8ea87fc82a7705
James Sherry <james.m.sherry@googlemail.com>
2017-11-19 02:09:43 +0000

    added eslint and tidied app.js; removed secrets to env vars

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+/* eslint-disable no-console, max-len, no-unused-vars */
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8553
-const request = require("request");
+const request = require('request');
 const childProcess = require('child_process');
 
-const clientId = '22CKYF';
-const secret = 'cb411d16a2e69e8d1481c69e8c7564dd';
-const headerRespone = clientId + ':' + secret;
+const port = process.env.PORT || 8553;
+
+const clientId = process.env.FITBIT_CLIENT_ID;
+const secret = process.env.FITBIT_SECRET;
+
+if (typeof clientId === 'undefined') {
+  throw new Error('NO_FITBIT_CLIENT_ID');
+}
+
+if (typeof secret === 'undefined') {
+  throw new Error('NO_FITBIT_SECRET');
+}
+
+const headerResponse = `${clientId}:${secret}`;
 
 
 // Set up the express app
@@ -21,49 +32,52 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup variables.
-var redirectURL = null;
+let redirectURL = null;
 
 // Check auth call works.
-app.get('/auth', (res, req) => {
-    console.log( 'AUTH is hit');
-    console.log( res );
+app.get('/auth', (req, res) => {
+  console.log('AUTH is hit');
+  console.log('response:\n\n', res);
 });
 
-//Server console
-app.use((req, res, next) => {
-    console.log('%s request to %s from %s', req.method, req.path, req.ip);
-    next();
-});
-
-//Server
-const server = app.listen(port, () => {
-    console.log(`rosa final project is working on port ${port}`);
-});
+// Server console
+// app.use((req, res, next) => {
+//     console.log('%s request to %s from %s', req.method, req.path, req.ip);
+//     next();
+// });
 
 // Token request - gets URL which we need to redirect the user to
 // so they can input their username and login details.
-app.get('/token', ( req, res) => {
-
-    const options = { method: 'POST',
+app.get('/token', (req, res) => {
+  const options = {
+    method: 'POST',
     url: 'https://www.fitbit.com/oauth2/authorize',
-    qs:
-    { response_type: 'token',
-    client_id: '22CKYF',
-    redirect_uri: '/auth',
-    scope: 'heartrate',
-    // scope: 'activity nutrition heartrate location nutrition profile settings sleep social weight',
-    expires_in: '604800' },
-    headers:
-    { 'Authorization': 'Basic ' + headerRespone.toString(64) } };
+    qs: {
+      response_type: 'token',
+      client_id: clientId,
+      redirect_uri: '/auth',
+      scope: 'heartrate',
+      // scope: 'activity nutrition heartrate location nutrition profile settings sleep social weight',
+      expires_in: '604800',
+    },
+    headers: {
+      Authorization: `Basic: ${headerResponse.toString(64)}`,
+    },
+  };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        //console.log(response);
-        //console.log(response.headers.location);
-        redirectURL = response.headers.location;
-        console.log(redirectURL);
-        //childProcess.exec('open -a "Google Chrome" ' + redirectURL);
-    });
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    // console.log(response);
+    // console.log(response.headers.location);
+    redirectURL = response.headers.location;
+    console.log('redirectURL', redirectURL);
+    childProcess.exec(`open -a "Google Chrome" ${redirectURL}`);
+  });
+});
+
+// Server
+const server = app.listen(port, () => {
+  console.log(`rosa final project is working on port ${port}`);
 });
 
 module.exports = server;
